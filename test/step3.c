66c27ac06ebab861b0911a510f2e8b33f0b8d4a1
kyuki3rain <kyuki3rain@gmail.com>
2023-08-22 17:43:16 +0900

    step3:complete

---
+++ test/step3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "util.h"
+#include "net.h"
+
+#include "driver/loopback.h"
+
+#include "test.h"
+
+static volatile sig_atomic_t terminate;
+
+static void on_signal(int s)
+{
+    (void)s;
+    terminate = 1;
+}
+
+int main(int argc, char *argv[])
+{
+    struct net_device *dev;
+
+    signal(SIGINT, on_signal);
+
+    // プロトコルスタックの初期化
+    if (net_init() != 0)
+    {
+        errorf("net_init() failed");
+        return -1;
+    }
+
+    // ダミーのネットワークデバイスを作成
+    dev = loopback_init();
+    if (!dev)
+    {
+        errorf("loopback_init() failed");
+        return -1;
+    }
+
+    // プロトコルスタックの起動
+    if (net_run() != 0)
+    {
+        errorf("net_run() failed");
+        return -1;
+    }
+
+    while (!terminate)
+    {
+        if (net_device_output(dev, 0x0800, test_data, sizeof(test_data), NULL) != 0)
+        {
+            errorf("net_device_output() failed");
+            return -1;
+        }
+        sleep(1);
+    }
+}
